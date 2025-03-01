ff7d4801cbfa9364b767ac6ec90c1f8717fe1fe2
Hieulecp <lehieucp2k3@gmail.com>
2025-03-01 10:24:15 +0700

    chỉnh sửa danh mục

--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -22,4 +22,14 @@ router.post(
     controller.createCategory
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+    "/edit/:id",
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.editItem
+);
+
 module.exports = router; 
