cec897559193db574272c73a64d22902a0e854c7
Hieulecp <lehieucp2k3@gmail.com>
2025-03-01 10:41:33 +0700

    chỉnh sửa danh mục

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,10 +1,12 @@
 const Product= require("../../models/product.model");
+const ProductCategory= require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHepler = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const createTree = require("../../helpers/createTree");
 
 
 // [GET] /admin/products
@@ -142,8 +144,19 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async (req, res) => {
+
+    let find= {
+        deleted : false
+    };
+
+    const category= await ProductCategory.find(find);
+
+    const newCategory= createTree.tree(category);
+    // console.log(newCategory);
+
     res.render("admin/pages/products/create", {
         pageTitle : "Thêm mới sản phẩm",
+        category: newCategory
     })
 };
 
--- models/product.model.js
+++ models/product.model.js
@@ -5,28 +5,34 @@ const slug = require('mongoose-slug-updater');
 mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema(
-{
-    title : String,
-    description: String,
-    price : Number,
-    discountPercentage : Number,
-    stock : Number,
-    thumbnail : String,
-    status : String,
-    position : Number,
-    slug : {
-        type: String,
-        slug: "title"
-    },
-    deleted : {
-        type: Boolean,
-        default: false,
-        unique: true
-    },
-    "deletedAt": Date
-}, {
-    timestamps : true
-});
+    {
+        title : String,
+        product_category_id:{
+            type: String,
+            default: ""
+        },
+        description: String,
+        price : Number,
+        discountPercentage : Number,
+        stock : Number,
+        thumbnail : String,
+        status : String,
+        position : Number,
+        slug : {
+            type: String,
+            slug: "title"
+        },
+        deleted : {
+            type: Boolean,
+            default: false,
+            unique: true
+        },
+        "deletedAt": Date
+    }, 
+    {
+        timestamps : true
+    }
+);
 
 const Product = mongoose.model('Product', productSchema, "products");
 
