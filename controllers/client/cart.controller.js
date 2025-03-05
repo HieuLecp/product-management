d847d448a5a24d291735e1cc4c498d9e27ed87d0
Hieulecp <lehieucp2k3@gmail.com>
2025-03-05 23:58:00 +0700

    Trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -3,7 +3,34 @@ const Cart= require("../../models/carts.model");
 
 const productHepler  = require("../../helpers/product");
 
+// [GET]/cart
+module.exports.index =  async (req, res) => {
+    const cartId= req.cookies.cartId;
+    
+    const cart= await Cart.findOne({_id: cartId});
+    console.log(cart);
 
+    if(cart.products.length > 0){
+        for(const item of cart.products){
+            const productId= item.product_id;
+            const productInfo= await Product.findOne({_id: productId});
+
+            productHepler.priceNewProduct(productInfo);
+
+            item.productInfo= productInfo;
+
+            item.totalPrice= item.quantity * productInfo.priceNew;
+            
+        }
+    }
+
+    cart.totalPrice= cart.products.reduce((sum, item) => sum + item.totalPrice , 0);
+
+    res.render("client/pages/cart/index", {
+        pageTitle : "Giỏ hàng",
+        cartDetail: cart
+    });
+};
 
 // [POST]/cart/add/:productId
 module.exports.addCart =  async (req, res) => {
