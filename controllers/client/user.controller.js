59535ce3a3442f478e8fe8b2f3e51f6ade9ebba5
Hieulecp <lehieucp2k3@gmail.com>
2025-03-06 20:24:54 +0700

    Đăng nhập client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -25,9 +25,18 @@ module.exports.registerPost =  async (req, res) => {
         email: req.body.email,
         deleted: false
     });
+    const exitsUsername= await User.findOne({
+        userName: req.body.userName,
+        deleted: false
+    });
 
     if(exitsEmail){
         req.flash("error", "email này đã tồn tại!");
+        res.redirect("back");
+        return;
+    }
+    if(exitsUsername){
+        req.flash("error", "tài khoản này đã tồn tại!");
         res.redirect("back");
         return;
     }
@@ -38,5 +47,45 @@ module.exports.registerPost =  async (req, res) => {
     console.log(user);
 
     res.cookie("tokenUser", user.tokenUser);
+    res.redirect("/");
+};
+
+// [GET] /user/login
+module.exports.login =  async (req, res) => {
+
+    res.render("client/pages/user/login.pug", {
+        pageTitle: "Trang đăng nhập",
+
+    });
+};
+
+// [POST] /user/login
+module.exports.loginPost =  async (req, res) => {
+    console.log(req.body);
+
+    const user= await User.findOne({
+        userName: req.body.userName
+    });
+
+    if(!user){
+        req.flash("error", "Tài khoản không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if(md5(req.body.password) != user.password){
+        req.flash("error", "Mật khẩu không chính xác!");
+        res.redirect("back");
+        return;
+    }
+
+    if(user.status == "inactive"){
+        req.flash("error", "Tài khoản đang bị khoá!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
+
     res.redirect("/");
 };
--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -32,3 +32,18 @@ module.exports.RegisterPatch = (req, res, next) => {
     
     next();
 };
+
+module.exports.loginPost= (req, res, next) => {
+    if(!req.body.userName){
+        req.flash("error", "Vui lòng nhập tên đăng nhập!");
+        res.redirect("back");
+        return;
+    }
+    if(!req.body.password){
+        req.flash("error", "Vui lòng nhập Mật khẩu!");
+        res.redirect("back");
+        return;
+    }
+    
+    next();
+}
