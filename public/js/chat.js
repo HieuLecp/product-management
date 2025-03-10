08e5ea7c9167d9b8efc29ffe6a1fe2f46df905e1
Hieulecp <lehieucp2k3@gmail.com>
2025-03-10 12:26:36 +0700

    thêm emoji vào chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,6 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
+
 // client_send_message
 const formSendData= document.querySelector(".chat .inner-form");
 if(formSendData){
@@ -30,10 +33,45 @@ socket.on("server_return_message", (data) => {
         htmlFullName= `<div class="inner-name">${data.fullName}</div>`
     }
     div.innerHTML= `
-        ${data.fullName}
+        ${htmlFullName}
         <div class="inner-content">${data.content}</div>
     `
 
     body.appendChild(div)
+
+    body.scrollTop = body.scrollHeight;
 })
 // end server_return_message
+
+// Scroll chat to bottom
+
+const bodyChat= document.querySelector(".chat .inner-body");
+if(bodyChat){
+    bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+
+// end Scroll chat to bottom
+
+// emoji
+const buttonIcon= document.querySelector(".button-icon");
+console.log(buttonIcon);
+if(buttonIcon){
+    const tooltip= document.querySelector(".tooltip");
+    Popper.createPopper(buttonIcon, tooltip)
+
+    buttonIcon.onclick= () => {
+        tooltip.classList.toggle('shown')
+    }
+}
+
+const emojiPicker= document.querySelector('emoji-picker')
+    if(emojiPicker){
+        const input= document.querySelector(".chat .inner-form input[name='content']");
+
+        emojiPicker.addEventListener("emoji-click", (e) => {
+            const icon= e.detail.unicode;
+            // console.log(icon);
+            input.value= input.value + icon;
+        })
+    }
+// end emoji
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,11 @@
 const socket= io();
 
+// const socket = io("ws://localhost:3000", {
+//     reconnection: true,        // Cho phép tự động kết nối lại
+//     reconnectionAttempts: 5,   // Số lần thử lại tối đa
+//     reconnectionDelay: 3000    // Thời gian chờ giữa các lần thử (3s)
+// });
+
 // let socket;
 
 // window.addEventListener("load", () => {
