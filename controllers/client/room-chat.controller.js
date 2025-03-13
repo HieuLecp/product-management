f2225e22287b2eb27ca4a508e5f12e978e1a0a0b
Hieulecp <lehieucp2k3@gmail.com>
2025-03-13 22:13:36 +0700

    fix

--- controllers/client/room-chat.controller.js
+++ controllers/client/room-chat.controller.js
@@ -5,29 +5,10 @@ const chatSocket  = require("../../socket/client/chat.socket");
 
 // [GET] /rooms-chat
 module.exports.index= async (req, res) => {
-    const roomChatId= req.params.roomChatId;
-    // console.log(roomChatId);
-
-    // socketIo
-        chatSocket(req, res);
-    // end socketIo
-
-    const chats= await Chat.find({
-        room_chat_id: roomChatId,
-        deleted: false
-    })
-
-    for(const chat of chats){
-        const infoUser= await User.findOne({
-            _id: chat.user_id
-        }).select("fullName");
-
-        chat.infoUser= infoUser;
-    }
+   
 
     res.render("client/pages/rooms-chat/index", {
         pageTitle: "Chat",
-        chats: chats
     })
 }
 
